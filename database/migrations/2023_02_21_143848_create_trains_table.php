7d9062a4fe0221f12408c49629cd38ca12f03f4b
Alexbarile <alexbarile93fg@yahoo.it>
2023-02-21 16:08:44 +0100

    creata la migration

--- database/migrations/2023_02_21_143848_create_trains_table.php
+++ database/migrations/2023_02_21_143848_create_trains_table.php
@@ -15,7 +15,16 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('stazione_partenza', 40);
+            $table->string('stazione_arrivo', 40);
+            $table->timeTz('orario_partenza');
+            $table->timeTz('orario_arrivo');
+            $table->string('codice_treno', 20);
+            $table->tinyInteger('numero_carrozze')->unsigned();
+            $table->tinyInteger('in_orario')->default(1);
+            $table->boolean('cancellato');
             $table->timestamps();
+
         });
     }
 
